4bcc9b93968af237c79b3f1712abaad739fbc285
Abinash <abinash.samal@techriff.in>
2022-07-19 13:35:53 +0530

    2nd change

--- Day1/multipleline.js
+++ Day1/multipleline.js
@@ -1,4 +1,10 @@
 let name ='Sam'
 let lastName = 'Samal'
 
-console.log(name,firstName, age, gravity, boilingPoint,)
+console.log(name,firstName, age, gravity, boilingPoint)
+
+let mAge='I am 25 years old'
+let yAge ='Your age is 30 years old'
+
+console.log (mAge)
+console.log(yAge)
