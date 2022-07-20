e08410f24136b86e1adcf9586c4b5e5a0bd7f3f5
Abinash <abinash.samal@techriff.in>
2022-07-20 12:25:39 +0530

    level-3 with changes

---
+++ Day3/Level_2/main.js
@@ -0,0 +1,28 @@
+let b=20
+let h=10
+
+const areaOfTriangle=0.5*b*h
+console.log(areaOfTriangle)
+
+let sideA=5
+let sideB=4
+let sideC=3
+const trianglePerimeter=sideA+sideB+sideC
+console.log(trianglePerimeter)
+
+let l=30
+let w=20
+const rectangleArea=l*w
+const perimeterRectangle= 2*(l+w)
+console.log(rectangleArea,perimeterRectangle)
+
+const pi=3.14
+let r= 5
+const circleArea=pi*r*r
+const circleCircumference=2*pi*r
+console.log(circleArea,circleCircumference)
+
+let x=10
+const yIntercept=2*x-2
+console.log(yIntercept)
+
