67545861c1ed1181a5000c5166d5e4fbc78f2f0a
Abinash <abinash.samal@techriff.in>
2022-07-20 06:23:21 +0530

    changes to level1

---
+++ Day2/Level_1/second.js
@@ -0,0 +1,34 @@
+let a='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(a.split(','))
+
+let b='30 Days Of Javascript'
+console.log(b.replace('Javascript','Python'))
+
+console.log(b.charAt(15))
+
+console.log(b.charCodeAt('J'))
+
+console.log(b.indexOf('a'))
+
+console.log(b.lastIndexOf('a'))
+
+let aA='You cannot end a sentence with because because because is a conjunction'
+console.log(aA.indexOf('because'))
+console.log(aA.lastIndexOf('because'))
+console.log(aA.search('because'))
+
+let aB='30 Days of JavaScript'
+console.log(aB.trim())
+
+console.log(aB.startsWith('30'))
+
+console.log(aB.endsWith('Script'))
+
+console.log(aB.match('a'))
+
+let aBC='30 Days Of'
+console.log(aBC.concat(' JavaScript'))
+
+let aBD='30 Days Of JavaScript'
+console.log(aBD.repeat(2))
+
