16202bf821655dbc7e90c7959da4418b712009a7
Abinash <abinash.samal@techriff.in>
2022-07-20 05:44:42 +0530

    first commit

---
+++ Day2/firstQ.js
@@ -0,0 +1,22 @@
+let string='30 Days Of JavaScript'
+console.log(string)
+
+console.log(string.length)
+
+let stringA='excuse me please'
+console.log(stringA.toUpperCase())
+
+let stringB='MY NAME IS'
+console.log(stringB.toLowerCase())
+
+let stringC='FootBall'
+console.log(stringC.substr(3,6))
+console.log(stringC.substring(4,7))
+
+let stringD='30 Days Of JavaScript'
+console.log(stringD.substring(3))
+console.log(stringD.substr(3,18))
+
+console.log(stringD.includes('Script'))
+
+
