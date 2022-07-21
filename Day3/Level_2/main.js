f08ac8dcc49d9c9bd8503c7e6518b148f8374630
Abinash <abinash.samal@techriff.in>
2022-07-21 10:40:24 +0530

    day4 added

--- Day3/Level_2/main.js
+++ Day3/Level_2/main.js
@@ -26,3 +26,46 @@ let x=10
 const yIntercept=2*x-2
 console.log(yIntercept)
 
+let y1=2
+y2=2
+x1=6
+x2=10
+const m=(y2-y1)*(x2-x1)
+console.log(m)
+
+let xA= -3
+const y= xA*xA + 6*xA + 9
+console.log(y)
+
+//9
+let ratePerHour=28
+hours=40
+const weeklyEarnig= ratePerHour * hours
+console.log(weeklyEarnig)
+
+//11
+
+let firstName='Abinash';
+let secondName='Samal';
+
+if (firstName.length > secondName.length) {
+    console.log("Your first name Abinash longer than family name Samal.");
+}
+
+
+
+//15
+const now = new Date()
+const year = now.getFullYear() 
+const month = now.getMonth() + 1 
+const date = now.getDate() 
+const hours1 = now.getHours() 
+const minutes = now.getMinutes() 
+
+console.log(`${year}-${month}-${date} ${hours1}:${minutes}`)
+
+console.log(`${date}-${month}-${year} ${hours1}:${minutes}`)
+
+console.log(`${date}/${month}/${year} ${hours1}:${minutes}`)
+
+
---
+++ Day4/main.js
@@ -0,0 +1,15 @@
+//1
+let age = prompt("Enter age:");
+
+switch (age) {
+  case '30':
+    console.log("You are old enough to drive");
+    break;
+
+  case '15':
+    console.log("wait for 3 more years");
+}
+
+//2
+
+
