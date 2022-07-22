78f541187d643200e9e3eef6564e637227aaa1b7
Abinash <abinash.samal@techriff.in>
2022-07-22 06:59:25 +0530

    started day-5

---
+++ Day5/main.js
@@ -0,0 +1,15 @@
+//1
+let arr=[]
+console.log(arr)
+
+//2
+let numbers=[1,2,3,4,5,6,7]
+console.log(numbers)
+
+//3
+console.log('No. of numbers:',numbers.length)
+
+//4
+firstIndex=numbers[0]
+console.log(firstIndex)
+console.log(numbers.lastIndexOf(7))
